f23e3c944fb4f3700ea8e1dc0282bc32e6bbe1c6
Roman A. <aksenov.nsk.r.a@gmail.com>
2024-11-06 20:56:11 +0200

    Done most part of func

--- main.py
+++ main.py
@@ -4,13 +4,40 @@ import sys
 def decipher_instructions(
         encrypted_instructions: str
         ) -> str:
-    pass
+    number_stack = []
+    string_stack = []
+    current_string = ""
+    current_number = 0
+
+    for char in encrypted_instructions:
+        if char.isdigit():
+            # Update current_number for repeat.
+            current_number = current_number * 10 + int(char)
+        elif char == '[':
+            # Save the current number and string to the stacks.
+            number_stack.append(current_number)
+            string_stack.append(current_string)
+
+            # Reset the current ones.
+            current_string = ""
+            current_number = 0
+        elif char == ']':
+            # Terminate the current string using the last
+            # number from the stack.
+            repeat_num = number_stack.pop()
+            last_str = string_stack.pop()
+            current_string = last_str + current_string * repeat_num
+        else:
+            # Add symbol to current_string.
+            current_string += char
+
+    return current_string
 
 
 def main():
-    cypher = sys.stdin.readline().rstrip()
+    cipher = sys.stdin.readline().rstrip()
     print(decipher_instructions(
-        encrypted_instructions=cypher))
+        encrypted_instructions=cipher))
 
 
 if __name__ == "__main__":
